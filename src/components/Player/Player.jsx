3417ba29bb257a30df6bb977c1eed9a23873f524
joynul24 <joynula919@gmail.com>
2025-01-08 03:35:41 +0600

    The work of making the card by loading the data is complete

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+
+const Player = ({ player }) => {
+  const {img, name, country, price, category, batting_hand } = player;
+  console.log(player);
+  return (
+
+    <div className="card border-2 border-gray-300 shadow-xl">
+      <figure className="h-60 px-6 pt-6">
+        <img
+          src={img}
+          alt="Shoes"
+          className="rounded-2xl w-full h-full object-cover"
+        />
+      </figure>
+      <div className="card-body space-y-4">
+        <h2 className="flex gap-4 items-center font-bold"><i className="fa-solid fa-user"></i>{name}</h2>
+        <div className="flex justify-between items-center opacity-70">
+            <h3 className="flex gap-4 items-center"><i className="fa-solid fa-flag"></i>{country}</h3>
+            <h3>{category}</h3>
+        </div>
+        <h3 className="font-bold">Rating</h3>
+        <div className="flex justify-between items-center font-bold">
+            <h3>Perfomance</h3>
+            <h3 className="opacity-70">{batting_hand}</h3>
+        </div>
+        {/* button */}
+        <div className="flex justify-between items-center">
+            <h3 className="font-semibold">Price: ${price}</h3>
+            <button className="btn btn-outline btn-secondary">Choose Player</button>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+Player.propTypes = {
+  player: PropTypes.object.isRequired,
+};
+
+export default Player;
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,28 +1,32 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Player from "../Player/Player";
 
-const players = () => {
+const Players = () => {
   const [players, setPlayers] = useState([]);
 
   useEffect(() => {
-    fetch('players.json')
-    .then(res => res.json())
-    .then(data => setPlayers(data))
-  },[])
+    fetch("players.json")
+      .then((res) => res.json())
+      .then((data) => setPlayers(data));
+  }, []);
 
   return (
-    <div className="mt-20">
+    <div className="mt-8">
       <div className="flex justify-between items-center">
-        <h3 className="text-3xl font-bold">Available Players {players.length}</h3>
-        <div>
-            <button className="py-4 px-7 font-bold">Available</button>
-            <button className="py-4 px-7 font-bold">Selected (0)</button>
+        <h2>Available Players</h2>
+        <div className="flex">
+          <button className="py-4 px-7">Available</button>
+          <button className="py-4 px-7">Selected (0)</button>
         </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 p-4 lg:p-0">
+        {players.map((player) => (
+          <Player key={player.id} player={player}></Player>
+        ))}
       </div>
     </div>
   );
 };
 
-
-
-export default players;
+export default Players;
